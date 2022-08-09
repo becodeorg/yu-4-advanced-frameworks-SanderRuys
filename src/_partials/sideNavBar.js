b24c7158b8343ac2d126bd2c6aa8a73eacc3c7e1
Sander Ruys <ruyssander@gmail.com>
2022-08-09 14:36:20 +0200

    Basic Navbar expands

--- src/_partials/sideNavBar.js
+++ src/_partials/sideNavBar.js
@@ -1,23 +1,26 @@
 import React from "react";
 import "../styles/sideNavBar.css";
 import logo from '../assets/icons/logo.svg'
+import { useState } from "react";
 
 const SideNavBar = () => {
+    const [isExpended, setExpendState] = useState(false);
     return (
-        <div className="sideNavBarContainer">
+        <div className={isExpended ? "sideNavBarContainer" : "sideNavBarContainer sideNavBarContainerNotExpanded"}>
             <div className="navUpper">
                 <div className="navHeading">
-                    <div className="navBrand">
-                        <img src={logo} alt="logo icon" />
-                        <h2>Showkart</h2>
-                    </div>
-                    <button className="hamburgerMenu">
+                    {isExpended && (
+                        <div className="navBrand">
+                            <img src={logo} alt="logo icon" />
+                            <h2>Showkart</h2>
+                        </div>
+                    )}
+                    <button className={isExpended ? "hamburgerMenu hamburgerMenuIn" : "hamburgerMenu hamburgerMenuOut"} onClick={() => setExpendState(!isExpended)}>
                         <span></span>
                         <span></span>
                         <span></span>
                     </button>
                 </div>
-
             </div>
         </div>
     );
