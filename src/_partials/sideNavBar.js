35c50aaae02b357c0c66bc486f4cc26ca3e90e9b
Sander Ruys <ruyssander@gmail.com>
2022-08-09 19:56:23 +0200

    Working side Navbar

--- src/_partials/sideNavBar.js
+++ src/_partials/sideNavBar.js
@@ -5,6 +5,28 @@ import { useState } from "react";
 
 const SideNavBar = () => {
     const [isExpended, setExpendState] = useState(false);
+    const menuItems = [
+        {
+            text: "Homepage",
+            icon: logo,
+        },
+        {
+            text: "Series",
+            icon: logo,
+        },
+        {
+            text: "Movies",
+            icon: logo,
+        },
+        {
+            text: "Categories",
+            icon: logo,
+        },
+        {
+            text: "Favorites",
+            icon: logo,
+        },
+    ];
     return (
         <div className={isExpended ? "sideNavBarContainer" : "sideNavBarContainer sideNavBarContainerNotExpanded"}>
             <div className="navUpper">
@@ -12,7 +34,7 @@ const SideNavBar = () => {
                     {isExpended && (
                         <div className="navBrand">
                             <img src={logo} alt="logo icon" />
-                            <h2>Showkart</h2>
+                            <h2>Snotflix</h2>
                         </div>
                     )}
                     <button className={isExpended ? "hamburgerMenu hamburgerMenuIn" : "hamburgerMenu hamburgerMenuOut"} onClick={() => setExpendState(!isExpended)}>
@@ -21,6 +43,24 @@ const SideNavBar = () => {
                         <span></span>
                     </button>
                 </div>
+                <div className="navMenu">
+                    {menuItems.map(({ text, icon }) => (
+                        <a href="#" className={isExpended ? "menuItem" : "menuItem menuItemNotExpended"}>
+                            <img src={icon} alt="" />
+                            {isExpended && <p>{text}</p>}
+                            {!isExpended && <div className="toolTip">{text}</div>}
+                        </a>
+                    ))}
+                </div>
+            </div>
+            <div className="navFooter">
+                {isExpended && (<div className="navDetails">
+                    <img src={logo} alt="logo icon" />
+                    <div className="navFooterInfo">
+                        <p className="navFooterUserName">Nick Drake</p>
+                    </div>
+                </div>)}
+                <img className="logoutIcon" src={logo} alt="logo icon" />
             </div>
         </div>
     );
